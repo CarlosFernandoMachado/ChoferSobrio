e7584c57a8ef772773fe0a4c2e039d4b4b8af534
Elmer Lopez <meme_lopez10@hotmail.com>
2019-05-22 15:09:24 -0600

    metí el dropdown dentro del div(toolbar_navigation-items)

--- chofer-sobrio/src/components/Toolbar/Toolbar.jsx
+++ chofer-sobrio/src/components/Toolbar/Toolbar.jsx
@@ -13,42 +13,22 @@ const toolbar = props => (
             </div>
             <div className="toolbar__logo"> <Link to="/">The Logo</Link> </div>
             <div className="spacer" />
-            {/* <div className="toolbar_navigation-items">
-                <ul>
-                    <Link to="/">
-                        <Button bsStyle="primary"> Home</Button>
-                    </Link>
-                    <Link to="/precios">
-                        <Button bsStyle="primary" > Precios</Button>
-                    </Link>
-                    <Link to="/seguridad">
-                        <Button bsStyle="primary"> Seguridad</Button>
-                    </Link>
-                    <Link to="/crear">
-                    <Button bsStyle="primary"> Crear </Button>
-                </Link>
-                    <Link to="/CrearGerente">
-                        <Button bsStyle="primary"> Crear Gerente</Button>
-                    </Link>
-                    <Link to="/pedidos">
-                        <Button bsStyle="primary"> Pedidos</Button>
-                    </Link>
-                </ul>
-            </div> */}
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Opciones
-                </Dropdown.Toggle>
+            <div className="toolbar_navigation-items">
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Opciones
+                    </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                    <Dropdown.Item><Link to="/"><Button bsStyle="primary">Home</Button> </Link></Dropdown.Item>
-                    <Dropdown.Item><Link to="/precios"><Button bsStyle="primary">Precios</Button></Link></Dropdown.Item>
-                    <Dropdown.Item><Link to="/seguridad"><Button bsStyle="primary">Seguridad </Button></Link></Dropdown.Item>
-                    <Dropdown.Item><Link to="/crear"><Button bsStyle="primary">Crear</Button></Link></Dropdown.Item>
-                    <Dropdown.Item><Link to="/CrearGerente"><Button bsStyle="primary">Crear Gerente</Button></Link></Dropdown.Item>
-                    <Dropdown.Item><Link to="/pedidos"><Button bsStyle="primary">Pedidos </Button></Link></Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+                    <Dropdown.Menu>
+                        <Dropdown.Item><Link to="/"><Button bsStyle="primary">Home</Button> </Link></Dropdown.Item>
+                        <Dropdown.Item><Link to="/precios"><Button bsStyle="primary">Precios</Button></Link></Dropdown.Item>
+                        <Dropdown.Item><Link to="/seguridad"><Button bsStyle="primary">Seguridad </Button></Link></Dropdown.Item>
+                        <Dropdown.Item><Link to="/crear"><Button bsStyle="primary">Crear</Button></Link></Dropdown.Item>
+                        <Dropdown.Item><Link to="/CrearGerente"><Button bsStyle="primary">Crear Gerente</Button></Link></Dropdown.Item>
+                        <Dropdown.Item><Link to="/pedidos"><Button bsStyle="primary">Pedidos </Button></Link></Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
         </nav>
     </header>
 );
