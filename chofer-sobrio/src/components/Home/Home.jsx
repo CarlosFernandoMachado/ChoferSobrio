bfb59c1c6bbf11e5a2048d670747a7ca2e8abca2
abialvarado14 <suyapa.caldex@hotmail.es>
2019-05-23 02:38:45 -0600

    Home, mapa

--- chofer-sobrio/src/components/Home/Home.jsx
+++ chofer-sobrio/src/components/Home/Home.jsx
@@ -6,17 +6,28 @@ import Map from '../Map/MapContainer'
 
 export default class Home extends Component {
     render() {
-        return (
+        return (                           
+
             <Container>
-                <Jumbotron>
-                    <h2>Bienvenidos a Chofer Sobrio</h2>
-                    <p>Esta es la pagina Home!</p>
-                </Jumbotron>
-                        <div id="mapa">
-                            <Map/>
-                        </div>
+                              
+                <div style={{ height: '330px', width: '82.5%', position: 'absolute', justifyContent: 'center'}}>  
+                    <div class="text-center">
+                         <Jumbotron fluid>               
+                            <h2>Bienvenidos a Chofer Sobrio</h2>
+                            <p>Esta es la pagina Home!</p>
+                         </Jumbotron>  
+                     </div>                       
+                    <Map />
+                    <div class="text-center">
+                        <Alert variant="warning">
+                                ¡Selecciona tu ubicación! 
+                        </Alert>
+                    </div>
+                </div>
+                        
+                        
                 <Link to="/pedirchofer">
-                    <div class="text-center" id="button">
+                    <div class="text-center" id="button" style={{ paddingTop:590}}>
                         <Button variant="warning" >Pedir Chofer</Button>
                     </div>
                 </Link>
