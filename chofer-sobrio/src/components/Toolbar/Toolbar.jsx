a2cb8ff0e2c98bfae506de2cf4b1e9cd5348243c
Karen Barrientos <karenp.bmedina@unitec.edu>
2019-05-23 14:22:03 -0600

    arreglar el menu

--- chofer-sobrio/src/components/Toolbar/Toolbar.jsx
+++ chofer-sobrio/src/components/Toolbar/Toolbar.jsx
@@ -5,33 +5,99 @@ import DrawerToggleButton from '../DrawerToggleButton/DrawerToggleButton';
 import { Link } from 'react-router-dom';
 import { Button, Dropdown } from 'react-bootstrap';
 
-const toolbar = props => (
-    <header className="toolbar">
-        <nav className="toolbar__navigation">
-            <div className="toolbar__toggle-button" >
-                <DrawerToggleButton click={props.drawerClickHandler} />
-            </div>
-            <div className="toolbar__logo"> <Link to="/">The Logo</Link> </div>
-            <div className="spacer" />
-            <div className="toolbar_navigation-items">
-                <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Opciones
-                    </Dropdown.Toggle>
+class Toolbar extends React.Component {
+    constructor(props) {
+        super(props);
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item><Link to="/"><Button >Home</Button> </Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/precios"><Button>Precios</Button></Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/seguridad"><Button>Seguridad </Button></Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/crear"><Button>Crear</Button></Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/CrearGerente"><Button>Crear Gerente</Button></Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/pedidos"><Button>Pedidos </Button></Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/iniciarSesion"><Button>Iniciar Sesión </Button></Link></Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            </div>
-        </nav>
-    </header>
-);
+        this.props = props;
 
-export default toolbar;
+        this.state = {
+            isAdmin: false,
+        };
+    }
+
+    componentDidMount() {
+        const user = JSON.parse(localStorage.getItem('user'));
+
+        if (user) {
+            // admins
+            this.dbRefAdmins = firebase.database().ref('/admin');
+            this.dbCallbackAdmins = this.dbRefAdmins.on('value', (snap) => {
+                const admins = snap.val();
+                admins.forEach(admin => this.setState({ isAdmin: admin.correo === user.email }));
+            });
+        }
+    }
+
+
+    componentWillUnmount() {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user) {
+            this.dbRefAdmins.off('value', this.dbCallbackAdmins);
+        }
+    }
+
+    render() {
+        const props = this.props;
+        const { isAdmin } = this.state;
+
+        let mensaje;
+        if (localStorage.getItem('user')) {
+            mensaje = 'Cerrar sesion';
+        } else {
+            mensaje = 'Iniciar sesion';
+        }
+
+        return (
+            <header className="toolbar">
+                <nav className="toolbar__navigation">
+                    <div className="toolbar__toggle-button" >
+                        <DrawerToggleButton click={props.drawerClickHandler} />
+                    </div>
+                    <div className="toolbar__logo"> <Link to="/">The Logo</Link> </div>
+                    <div className="spacer" />
+                    <div className="toolbar_navigation-items">
+                        <Dropdown>
+                            <Dropdown.Toggle vari="dropdown-basic">
+                                Opciones
+                            </Dropdown.Toggle>
+        
+                            <Dropdown.Menu>
+                                <Dropdown.Item><Link to="/"><Button >Home</Button> </Link></Dropdown.Item>
+                                <Dropdown.Item><Link to="/precios"><Button>Precios</Button></Link></Dropdown.Item>
+                                <Dropdown.Item><Link to="/seguridad"><Button>Seguridad </Button></Link></Dropdown.Item>
+                                <Dropdown.Item><Link to="/crear"><Button>Crear</Button></Link></Dropdown.Item>
+
+                                {/* Menus de admin */}
+                                {isAdmin ? (
+                                    <React.Fragment>
+                                        <Dropdown.Item>
+                                            <Link to="/CrearGerente">
+                                                <Button bsStyle="primary"> Crear Gerente</Button>
+                                            </Link>
+                                        </Dropdown.Item>
+                                        <Dropdown.Item>
+                                            <Link to="/CrearChofer">
+                                                <Button bsStyle="primary"> Crear Chofer</Button>
+                                            </Link>
+                                        </Dropdown.Item>
+                                        <Dropdown.Item>
+                                            <Link to="/CrearCliente">
+                                                <Button bsStyle="primary"> Crear Cliente</Button>
+                                            </Link>
+                                        </Dropdown.Item>
+                                    </React.Fragment>
+                                ) : null}
+
+                                <Dropdown.Item><Link to="/pedidos"><Button>Pedidos </Button></Link></Dropdown.Item>
+                                <Dropdown.Item><Link to="/iniciarSesion"><Button>Iniciar Sesión </Button></Link></Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                </nav>
+            </header>
+        );
+    }
+};
+
+export default Toolbar;
