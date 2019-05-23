93fa107ad639fdfd1f1c06152a42fc274034773c
CarlosFernandoMachado <testsubjectno2@hotmail.com>
2019-05-23 14:43:25 -0600

    Oops
    otro pequeño bug

--- chofer-sobrio/src/components/Home/Home.jsx
+++ chofer-sobrio/src/components/Home/Home.jsx
@@ -6,31 +6,30 @@ import Map from '../Map/MapContainer'
 
 export default class Home extends Component {
     render() {
-        return (                           
+        return (
 
             <Container>
-                              
-                <div style={{ height: '330px', width: '82.5%', position: 'absolute', justifyContent: 'center'}}>  
+
+                <div style={{ height: '330px', width: '82.5%', position: 'absolute', justifyContent: 'center', display: 'inline-block' }}>
                     <div class="text-center">
-                         <Jumbotron fluid>               
+                        <Jumbotron fluid>
                             <h2>Bienvenidos a Chofer Sobrio</h2>
                             <p>Esta es la pagina Home!</p>
-                         </Jumbotron>  
-                     </div>                       
+                        </Jumbotron>
+                    </div>
                     <Map />
                     <div class="text-center">
                         <Alert variant="warning">
-                                ¡Selecciona tu ubicación! 
+                            ¡Selecciona tu ubicación!
                         </Alert>
                     </div>
                 </div>
-                        
-                        
-                <Link to="/pedirchofer">
-                    <div class="text-center" id="button" style={{ paddingTop:590}}>
-                        <Button variant="warning" >Pedir Chofer</Button>
-                    </div>
-                </Link>
+                
+                    <Link to="/pedirchofer">
+                        <div class="text-center" id="button" style={{ paddingTop: 590 }}>
+                            <Button variant="warning" >Pedir Chofer</Button>
+                        </div>
+                    </Link>
             </Container>
         )
     }
