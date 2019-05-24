0d1e8f1137d95601d2ac3d49071941d11ad696a5
Elmer Lopez <meme_lopez10@hotmail.com>
2019-05-23 22:06:35 -0600

    hola

--- chofer-sobrio/src/components/Toolbar/Toolbar.jsx
+++ chofer-sobrio/src/components/Toolbar/Toolbar.jsx
@@ -14,15 +14,23 @@ const toolbar = props => (
             <div className="toolbar__logo"> <Link to="/">The Logo</Link> </div>
             <div className="spacer" />
             <div className="toolbar_navigation-items">
+                <ul>
+                    <Link to="/">
+                        <Button bsStyle="primary"> Home</Button>
+                    </Link>
+                    <Link to="/precios">
+                        <Button bsStyle="primary" > Precios</Button>
+                    </Link>
+                    <Link to="/seguridad">
+                        <Button bsStyle="primary"> Seguridad</Button>
+                    </Link>
+                </ul>
                 <Dropdown>
                     <Dropdown.Toggle variant="success" id="dropdown-basic">
                         Opciones
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu>
-                        <Dropdown.Item><Link to="/"><Button >Home</Button> </Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/precios"><Button>Precios</Button></Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/seguridad"><Button>Seguridad </Button></Link></Dropdown.Item>
                         <Dropdown.Item><Link to="/crear"><Button>Crear</Button></Link></Dropdown.Item>
                         <Dropdown.Item><Link to="/CrearGerente"><Button>Crear Gerente</Button></Link></Dropdown.Item>
                         <Dropdown.Item><Link to="/pedidos"><Button>Pedidos </Button></Link></Dropdown.Item>
